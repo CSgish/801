743c8346efcdbaf4b07437e202ab0fd568e105db
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-09-21 07:57:15 +0800

    Create 作業三範例2.cpp

---
+++ 作業三範例2.cpp
@@ -0,0 +1,19 @@
+//70808吳仲倫
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float classNo;
+    int number;
+    char gender;
+    string name;
+    
+    cout<<"班級 "<<endl;
+    cout<<"座號 "<<endl;
+    cout<<"性別 "<<endl;
+    cout<<"姓名 "<<endl;
+    
+}
