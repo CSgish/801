81cef588d1e17654cc69ddece334abc031e59c80
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-10-21 14:44:13 +0800

    Create 加分題.cpp

---
+++ 加分題.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string = ;      //請將變數設為account並加上預設值
+    string = ;      //請將變數設為password並加上預設值
+    string A,P;     
+    
+    //輸入
+    cout<<"帳號：";
+    cin;                //輸入到A
+    cout<<"密碼：";
+    cin;                //輸入到P
+    
+    //測驗帳號密碼
+    
+    //if
+    cout<<"登入成功！\n"<<endl;
+    //else
+    cout<<"登入失敗！\n"<<endl;
+    
+}
