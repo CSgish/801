22e3435f4cf537e23493c836535960b83e61d4b0
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-10-21 14:39:08 +0800

    Create 作業七範例(可以複製).cpp

---
+++ 作業七範例(可以複製).cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //設定變數
+    int ;       //設定為x         
+    int ;      //設定為y
+    
+    //輸入
+    cin;
+
+    //條件判斷
+    
+    //if                  
+    cout<<"x=y"<<endl;
+    
+    //else if             
+    cout<<"x<y"<<endl;
+    
+    //else
+    cout<<"x>y"<<endl;
+    
+}
