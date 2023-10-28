1b735690faeee43418560c24830a1dee23d9eb1a
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-10-28 14:21:10 +0800

    Create 作業八範例(可複製).cpp

---
+++ 作業八範例(可複製).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string = ;      //請將變數設為account並加上預設值
+    string = ;      //請將變數設為password並加上預設值
+    string A,P;     
+    
+    //輸入
+    cout<<"帳號：";
+    cin;                //輸入到A
+    cout<<"密碼：";
+    cin;                //輸入到P
+    
+    //測驗帳號密碼
+    
+    if                                        //帳號密碼都正確
+        cout<<"登入成功！"<<endl;
+    else if                                   //帳號或者密碼錯誤
+        cout<<"帳號或密碼錯誤"<<endl;
+    else
+        cout<<"登入失敗！"<<endl;
+    
+}
