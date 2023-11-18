630586cf273a4dda057228a798adab2ca17f74c0
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2023-11-18 15:11:34 +0800

    Update 加分題.cpp

--- 加分題.cpp
+++ 加分題.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 int main()
 {
-    //設定變數
     int min = 1;
     int max = 100;
     int input = -1;
-    int answer = ;  //自己決定答案要是多少
-    
-    while(){        //請加上條件(答案!=猜的數字)
-        
-        //輸入
+    int answer = ;  //自己決定
+    int count=0;
+    cout<<"範圍：1~100"<<endl;
+    for(){          //限定只能猜五次
+        count++;
         cout<<"請輸入想猜的數字：";
         cin>>input;
-        
-        //邏輯判斷
         if(input>max||input<min){
             cout<<"不在範圍內"<<endl;
         }
-        else if(input==answer){
+        else if(){
+            min = ;
+            cout<<"範圍："<<min<<"~"<<max<<endl;
+        }
+        else if(){
             cout<<"恭喜答對！"<<endl;
+            break;
         }
-        else if(){          
-            min = ;     //更改範圍
-            cout<<"範圍："<<min<<"~"<<max<<endl;
-        }
-        
         else{
-            max = ;     //更改範圍
+            max=;
             cout<<"範圍："<<min<<"~"<<max<<endl;
         }
-        
     }
+    if(count>=5){
+        cout<<"很遺憾次數用盡了"<<endl;
+    }
+        
 }
