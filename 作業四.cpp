e250869b8fb51eaf3b4821dc56944d3cbdcc6800
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2024-03-13 19:26:17 +0800

    Create 作業四.cpp

---
+++ 作業四.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Student{
+    public:
+           
+         
+            
+            
+};
+int main()
+{
+    Student Wang;
+    
+    
+    
+    
+    cout<<Wang.name;
+    
+}
