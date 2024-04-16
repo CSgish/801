b25f7a3eccf7a484945c2134539f7cd2a22eb548
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2024-04-16 18:36:51 +0800

    Create 作業六.cpp

---
+++ 作業六.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[]){
+    int temp;  
+    for(){
+        for(){
+            if(){
+                
+            }       
+        }
+    }
+}
+int main()
+{
+    int arr[5]={1,5,3,4,2};
+    bubbleSort(arr);
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }    
+}
