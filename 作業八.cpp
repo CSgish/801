b41f20cd1a68e2bcf0c9956107ddacd1b626de8c
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2024-05-21 10:14:50 +0800

    Create 作業八.cpp

---
+++ 作業八.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void insertionSort(int arr[]){
+    int key;    
+    for(){   //第0格默認是排序好的狀態，從第1格開始
+        key=arr[i];         //目前要插入的數字
+        for(){
+            if(){
+                
+            }
+            else{
+                
+                
+            }
+        }   
+    }
+}
+int main()
+{
+    int arr[5]={1,5,3,4,2};
+    insertionSort(arr);
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }    
+}
