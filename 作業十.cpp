83509792b43cc0a885e2da1b4489d6955a19e8ea
ZhungLun <114723448+zhunglun0428@users.noreply.github.com>
2024-06-04 08:53:02 +0800

    Update and rename 作業八.cpp to 作業十.cpp

---
+++ 作業十.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void selectionSort(int arr[]){
+    int min;
+    int minPos;
+    int temp;
+    for(){
+        
+        
+        for(){
+            
+                
+                
+            }
+        }
+        
+        
+        
+    }
+}
+
+int main()
+{
+    int arr[6]={3,5,2,8,9,4};
+    selectionSort(arr);
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }    
+}
